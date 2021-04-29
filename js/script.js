931b0aa408be854996d1858111940328088cc55e
Ali <galaxymexx@gmail.com>
2021-04-30 01:50:01 +0600

    add Forms

--- js/script.js
+++ js/script.js
@@ -154,8 +154,8 @@ window.addEventListener('DOMContentLoaded', () => {
         // Modal
 
         const modalTrigger = document.querySelectorAll('[data-modal]'),
-                modal = document.querySelector('.modal'),
-                closeModalBtn = modal.querySelector('[data-close]');
+                modal = document.querySelector('.modal');
+               
 
         function openModal() {
             modal.classList.add('show');
@@ -175,11 +175,10 @@ window.addEventListener('DOMContentLoaded', () => {
             modal.classList.remove('show');
             document.body.style.overflow = '';
         }
-
-        closeModalBtn.addEventListener('click', closeModal);
         
+
         modal.addEventListener('click', (e) => {
-            if (e.target && e.target === modal) {
+            if (e.target && e.target === modal || e.target.getAttribute('data-close') == '') {
                 closeModal();
             }
         });
@@ -202,7 +201,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
         // Menu Cards
-
         class MenuCard {
             constructor(src, alt, title, descr, price, parentSelector, ...classes) {
                 this.src = src;
@@ -273,5 +271,76 @@ window.addEventListener('DOMContentLoaded', () => {
             '.menu .container',
             'menu__item'
         ).render();
-});
+        
+        //Forms
 
+        const forms = document.querySelectorAll('form');
+
+        const message = {
+            loading: 'img/spinner.svg',
+            succes: 'Спасибо! Скоро мы с вами свяжемся.',
+            failure: 'Что-то пошло не так...',
+        };
+
+        forms.forEach(item => {
+            postData(item);
+        });
+
+        function postData(form) {
+            form.addEventListener('submit', (e) => {
+                e.preventDefault();
+
+                const statusMessage = document.createElement('img');
+                statusMessage.src = message.loading;
+                statusMessage.style.cssText = `
+                    display: block;
+                    margin: 0 auto;
+                `;
+                form.insertAdjacentElement('afterend', statusMessage);
+                
+                const request = new XMLHttpRequest();
+                request.open('POST', 'server.php');
+                request.setRequestHeader('Content-type', 'application/json');
+                const formData = new FormData(form);
+                const obj = {};
+                formData.forEach(function(value, key){
+                    obj[key] = value;
+                });
+                const json = JSON.stringify(obj);
+                request.send(json);
+
+                request.addEventListener('load', () => {
+                    if(request.status === 200) {
+                        console.log(request.response);
+                        showThanksModal(message.succes);
+                        form.reset();
+                        statusMessage.remove();
+                    } else {
+                        showThanksModal(message.failure);
+                    }
+                });
+
+            });
+        }
+
+        function showThanksModal(message) {
+            const prevModalDialog = document.querySelector('.modal__dialog');
+            prevModalDialog.classList.add('hide');
+            openModal();
+            const thanksModal = document.createElement('div');
+            thanksModal.classList.add('modal__dialog');
+            thanksModal.innerHTML = `
+                <div class="modal__content">
+                    <div data-close class="modal__close">&times;</div>
+                    <div class="modal__title">${message}</div>
+                </div>
+            `;
+            document.querySelector('.modal').append(thanksModal);
+            setTimeout(() => {
+                thanksModal.remove();
+                prevModalDialog.classList.add('show');
+                prevModalDialog.classList.remove('hide');
+                closeModal();
+            }, 4000);
+        }
+});

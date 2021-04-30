907fd646477ca993bd0ad2ad2a7cf9ca39c20d25
Ali <galaxymexx@gmail.com>
2021-04-30 20:07:05 +0600

    using Fetch

--- js/script.js
+++ js/script.js
@@ -242,35 +242,22 @@ window.addEventListener('DOMContentLoaded', () => {
             }
         }
 
-        new MenuCard(
-            'img/tabs/vegy.jpg',
-            'vegy',
-            'Меню "Фитнес',
-            'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-            2, 
-            '.menu .container',
-            'menu__item'
-        ).render();
+        const getResource = async (url) => {
+            const res = await fetch(url);
 
-        new MenuCard(
-            'img/tabs/elite.jpg',
-            'elite',
-            'Меню “Премиум”',
-            'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-            3, 
-            '.menu .container',
-            'menu__item'
-        ).render();
+            if (!res.ok) {
+                throw new Error(`Could not fetch ${url}, status ${res.status}`);
+            }
 
-        new MenuCard(
-            'img/tabs/post.jpg',
-            'post',
-            'Меню "Постное"',
-            'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-            2, 
-            '.menu .container',
-            'menu__item'
-        ).render();
+            return await res.json();
+        };
+
+        getResource('http://localhost:3000/menu')
+        .then(data => {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container', 'menu__item').render();
+            });
+        });
         
         //Forms
 
@@ -283,10 +270,22 @@ window.addEventListener('DOMContentLoaded', () => {
         };
 
         forms.forEach(item => {
-            postData(item);
+            bindPostData(item);
         });
 
-        function postData(form) {
+        const postData = async (url, data) => {
+            const res = await fetch(url, {
+                method: 'POST',
+                    headers: {
+                        'Content-type': 'application/json',
+                    },
+                body: data
+            });
+
+            return await res.json();
+        };
+
+        function bindPostData(form) {
             form.addEventListener('submit', (e) => {
                 e.preventDefault();
 
@@ -297,29 +296,20 @@ window.addEventListener('DOMContentLoaded', () => {
                     margin: 0 auto;
                 `;
                 form.insertAdjacentElement('afterend', statusMessage);
-                
-                const request = new XMLHttpRequest();
-                request.open('POST', 'server.php');
-                request.setRequestHeader('Content-type', 'application/json');
+
                 const formData = new FormData(form);
-                const obj = {};
-                formData.forEach(function(value, key){
-                    obj[key] = value;
-                });
-                const json = JSON.stringify(obj);
-                request.send(json);
+                const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-                request.addEventListener('load', () => {
-                    if(request.status === 200) {
-                        console.log(request.response);
-                        showThanksModal(message.succes);
-                        form.reset();
-                        statusMessage.remove();
-                    } else {
-                        showThanksModal(message.failure);
-                    }
+                postData('http://localhost:3000/requests', json)
+                .then(data => {
+                    console.log(data);
+                    showThanksModal(message.succes);
+                    statusMessage.remove();
+                }).catch(() => {
+                    showThanksModal(message.failure);
+                }).finally(() => {
+                    form.reset();
                 });
-
             });
         }
 
@@ -343,4 +333,8 @@ window.addEventListener('DOMContentLoaded', () => {
                 closeModal();
             }, 4000);
         }
+
+        fetch('http://localhost:3000/menu')
+            .then(data => data.json())
+            .then(res => console.log(res));
 });
